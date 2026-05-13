508037eefd86228379a7be5cb8bd914087ee9f3b
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-14 00:09:28 +0200

    chore(devshell): remove clutter

--- nix/devshells.nix
+++ nix/devshells.nix
@@ -1,18 +1,15 @@
 {
   perSystem =
-    { self', pkgs, ... }:
+    { pkgs, ... }:
     {
       devShells.default = pkgs.mkShell {
-        inputsFrom = [ self'.packages.default ];
-        RUST_SRC_PATH = toString pkgs.rustPlatform.rustLibSrc;
-        buildInputs = with pkgs; [
+        packages = with pkgs; [
           cargo
           rustc
           rust-analyzer
           rustfmt
           clippy
           reuse
-          rustPlatform.rustLibSrc
         ];
       };
     };
